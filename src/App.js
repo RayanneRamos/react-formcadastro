aae0dd8cfbe1b8e8d354006272b26c52d8205d17
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-04 16:03:03 -0300

    Criação da função de validar CPF

--- src/App.js
+++ src/App.js
@@ -7,13 +7,21 @@ function App() {
   return (
     <Container component='article' maxWidth='sm'>
       <Typography variant='h3' component='h1' align='center'>Formulário Cadastro</Typography>
-      <FormularioCadastro aoEnviar={aoEnviarForm} />
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
     </Container>
   );
 }
 
 function aoEnviarForm(dados) {
   console.log(dados);
+}
+
+function validarCPF(cpf) {
+  if(cpf.length !== 11) {
+    return { valido: false, texto: 'CPF deve ter 11 dígitos' }
+  } else {
+    return { valido: true, texto: '' }
+  }
 }
 
 export default App;
