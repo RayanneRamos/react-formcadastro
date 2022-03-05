ff0a1b7ba6eabc98a8b285bdcd6b9e92cad0d36d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-05 12:07:32 -0300

    Criando os estados para o componente DadosEntrega

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,39 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
 
-const DadosEntrega = () => {
+const DadosEntrega = ({ aoEnviar }) => {
+  const [ cep, setCEP ] = useState('');
+  const [ endereco, setEndereco ] = useState('');
+  const [ numero, setNumero ] = useState('');
+  const [ estado, setEstado ] = useState('');
+  const [ cidade, setCidade ] = useState('');
+
   return(
-    <form>
+    <form 
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ cep, endereco, numero, cidade, estado });
+      }}
+    >
       <TextField 
+        value={cep}
+        onChange={(event) => setCEP(event.target.value)}
         id='cep'
         label='CEP'
         type='number'
         variant='outlined'
         margin='normal'
       />
-      <TextField 
+      <TextField
+        value={endereco}
+        onChange={(event) => setEndereco(event.target.value)} 
         id='endereco'
-        label='Endereco'
+        label='Endereço'
         type='text'
         variant='outlined'
         margin='normal'
         fullWidth
       />
-      <TextField 
+      <TextField
+        value={numero}
+        onChange={(event) => setNumero(event.target.value)} 
         id='numero'
         label='Numero'
         type='number'
         variant='outlined'
         margin='normal'
       />
-      <TextField 
+      <TextField
+        value={estado}
+        onChange={(event) => setEstado(event.target.value)} 
         id='estado'
         label='Estado'
         type='text'
         variant='outlined'
         margin='normal'
       />
-      <TextField 
+      <TextField
+        value={cidade}
+        onChange={(event) => setCidade(event.target.value)} 
         id='cep'
         label='Cidade'
         type='text'
