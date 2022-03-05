0d7bbc6435eb05eb985f91f53c00d926ddf531c6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-05 12:42:36 -0300

    Retirando a função validarCPF do app.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import { Container, Typography  } from '@mui/material';
 import 'fontsource-roboto';
+import { validarCPF, validarSenha } from './models/cadastro';
 
 function App() {
   return (
     <Container component='article' maxWidth='sm'>
       <Typography variant='h3' component='h1' align='center'>Formulário Cadastro</Typography>
-      <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+      <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{ cpf: validarCPF, senha: validarSenha}} />
     </Container>
   );
 }
 
 function aoEnviarForm(dados) {
   console.log(dados);
-}
-
-function validarCPF(cpf) {
-  if(cpf.length !== 11) {
-    return { valido: false, texto: 'CPF deve ter 11 dígitos' }
-  } else {
-    return { valido: true, texto: '' }
-  }
 }
 
 export default App;
