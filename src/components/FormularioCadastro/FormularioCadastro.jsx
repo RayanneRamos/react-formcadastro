cb643af176001bc9a5d5b54352195bf782e2d3fe
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-05 12:22:53 -0300

    Criando os steps de navegação no componente FormularioCadastro

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,20 +2,23 @@ import React, { useState, useEffect } from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
+import { Typography, Stepper, Step, StepLabel } from '@mui/material';
 
 const FormularioCadastro = ({ aoEnviar, validarCPF }) => {
   const [ etapaAtual, setEtapaAtual ] = useState(0);
   const [ dadosColetados, setDadosColetados ] = useState({});
   
   useEffect(() => {
-    console.log(dadosColetados);
+    if(etapaAtual === formularios.length - 1) {
+      aoEnviar(dadosColetados);
+    }
   });
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados} />,
     <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={coletarDados} />
-
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant='h5'>Obrigado por se cadastrar!</Typography>
   ];
 
   function coletarDados(dados) {
@@ -29,6 +32,20 @@ const FormularioCadastro = ({ aoEnviar, validarCPF }) => {
 
   return (
     <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
       {
         formularios[etapaAtual]
       }
