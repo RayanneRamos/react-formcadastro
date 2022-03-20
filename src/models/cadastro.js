6e8f871dce23ef258c39d8bf74169be713d5135c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-20 16:19:19 -0300

    Refatorando o if

--- src/models/cadastro.js
+++ src/models/cadastro.js
@@ -1,6 +1,6 @@
 function validarCPF(cpf) {
   if(cpf.length !== 11) {
-    return { valido: false, texto: 'CPF deve ter 11 dígitos' }
+    return { valido: false, texto: 'CPF deve ter 11 dígitos.' }
   } else {
     return { valido: true, texto: '' }
   }
@@ -8,7 +8,7 @@ function validarCPF(cpf) {
 
 function validarSenha(senha) {
   if(senha.length < 4 || senha.length > 72) {
-    return { valido: false, texto: 'Senha deve ter entre 4 e 72 dígitos' }
+    return { valido: false, texto: 'Senha deve ter entre 4 e 72 dígitos.' }
   } else {
     return { valido: true, texto: '' }
   }
